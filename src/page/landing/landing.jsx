f10ebf6dcbe51c9febe7025a0cb85eff294cf723
iman-skandari <iimisk9919@gmail.com>
2023-07-15 15:29:25 +0330

    setup pr

---
+++ src/page/landing/landing.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from"../../components/navbar"
+
+
+
+
+
+
+export default  () => {
+  return (
+    <div>
+        <Navbar/>
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
