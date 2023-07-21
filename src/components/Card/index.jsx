74ee2ad2f5a535d2073c6b62cf77ce46bb31bdda
iman-skandari <iimisk9919@gmail.com>
2023-07-21 12:41:10 +0330

    create components card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import "./card.css"
+import food from"../../assets/food.PNG"
+
+
+
+
+
+export default  () => {
+  return (
+
+
+    <div>
+
+
+       <div className='cardTitle'>
+        
+        <div className='cardTitleBox'>
+           <img src={food} alt="" />
+         
+        
+        </div>
+        <div className=' cardTitleP'><p>غذای اصلی</p></div>
+         
+
+
+       </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
