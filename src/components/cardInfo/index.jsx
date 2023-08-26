f92d5a4272ea4ba91346358360d32085b1ccdbf9
iman-skandari <iimisk9919@gmail.com>
2023-08-26 14:45:26 +0330

    add to component cardInfo

--- src/components/cardInfo/index.jsx
+++ src/components/cardInfo/index.jsx
@@ -1,32 +1,51 @@
 import React from "react";
 import "./cardInfo.css";
-import image from "../../assets/Image.png";
-import arow from "../../assets/arrow-left.png"
+import image from "../../assets/image.svg";
+import arow from "../../assets/arrow-left.png";
+import { Row, Col } from "antd";
+import image1 from "../../assets/image1.png";
+import image2 from "../../assets/image2.png";
 export default () => {
   return (
     <div className="cardInfo">
-      <div className="cardInfo1">
-        <img src={image} alt="" />
-        <h2> شعبه اکباتان</h2>
-        <p>شهرک اکباتان فاز3،بلوک 8</p>
-      </div>
-      <div className="cardInfo1">
-        <img src={image} alt="" />
-        <h2> شعبه چالوس</h2>
-        <p>چالوس خیابان 17 فروردین،بعد از کوچه کوروش، جنب داروخونه میلانی</p>
-      </div>
-      <div className="cardInfo1">
-        <img src={image} alt="" />
-        <h2> شعبه اقدسیه</h2>
-        <p>خیابان اقدسیه نرسیده به خیابان خیام</p>
-      </div>
-      <div className="cardInfo2">
-        <img src={image} alt="" />
-        <h2> شعبه ونک</h2>
-        <br />
-        <p>میدون ونک خیابان فردوسی نبش کوچه نیلوفر</p>
-        {/* <button className="cardInfoBtn"><p>صفحه شعبه</p> <img src={arow} alt="" /></button> */}
-      </div>
+      <Row gutter={[16, 16]}>
+        <Col span={6}>
+          <div className="cardInfo1">
+            <img src={image1} alt="" /> <h3>شعبه اکباتان</h3>
+            <p>شهرک اکباتان فاز3،مجتمع تجاری کوروش،طبقه سوم</p>
+          </div>
+        </Col>
+
+        <Col span={6}>
+          <div className="cardInfo2">
+            <img src={image1} alt="" />
+            <h3>شعبه چالوس</h3>
+            <p>
+              خیابان چالوس، خیابان17شهریور، بعد از کوچه کوروش، جنب داروخانه دکتر
+              میلانی
+            </p>
+          </div>
+        </Col>
+
+        <Col span={6}>
+          <div className="cardInfo3">
+            <img src={image} alt="" />
+            <h3>شعبه اقدسیه</h3>
+            <p>خیابان اقدسیه نرسیده به میدان خیام</p>
+          </div>
+        </Col>
+
+        <Col span={6}>
+          <div className="cardInfo4">
+            <img className="cardInfo4Img" src={image2} alt="" />
+            <h3>شعبه ونک</h3>
+            <p>میدان ونک خیابان فردوسی</p>
+            <button className="cardInfoBtn">
+              <p>صفحه شعبه</p> <img src={arow} alt="" />
+            </button>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
